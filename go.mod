9f9fa0afc3b91214e711cedcdff44dad9a7b1708
bwangelme <bwangel.me@gmail.com>
2020-11-17 23:19:48 +0800

    Feat: add stringsvc4

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.15
 
 require (
 	github.com/go-kit/kit v0.10.0
+	github.com/nats-io/jwt v1.2.0 // indirect
+	github.com/nats-io/nats.go v1.10.0 // indirect
 	github.com/prometheus/client_golang v1.8.0
 	github.com/sony/gobreaker v0.4.1
+	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
 )
 
---
+++ stringsvc4/main.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	natstransport "github.com/go-kit/kit/transport/nats"
+	"github.com/nats-io/nats.go"
+	log "github.com/sirupsen/logrus"
+)
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+var ErrEmpty = errors.New("empty string")
+
+type stringService struct {
+}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+type ServiceMiddleware func(StringService) StringService
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+func makeUppercaseHTTPEndpoint(nc *nats.Conn) endpoint.Endpoint {
+	return natstransport.NewPublisher(
+		nc,
+		"stringsvc.uppercase",
+		natstransport.EncodeJSONRequest,
+		decodeUppercaseResponse,
+	).Endpoint()
+}
+
+func decodeUppercaseResponse(_ context.Context, msg *nats.Msg) (interface{}, error) {
+	var response uppercaseResponse
+
+	log.WithFields(log.Fields{
+		"name": "decodeUppercaseResponse",
+		"data": string(msg.Data),
+	}).Info()
+	if err := json.Unmarshal(msg.Data, &response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func decodeUppercaseHTTPRequest(_ context.Context, req *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(req.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeUppercaseRequest(_ context.Context, req *nats.Msg) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.Unmarshal(req.Data, &request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func makeCountHTTPEndpoint(nc *nats.Conn) endpoint.Endpoint {
+	return natstransport.NewPublisher(
+		nc,
+		"stringsvc.count",
+		natstransport.EncodeJSONRequest,
+		decodeCountResponse,
+	).Endpoint()
+}
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{V: v, Err: ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		n := svc.Count(req.S)
+		return countResponse{n}, nil
+	}
+}
+
+func decodeCountResponse(_ context.Context, msg *nats.Msg) (interface{}, error) {
+	var respone countResponse
+	if err := json.Unmarshal(msg.Data, &respone); err != nil {
+		return nil, err
+	}
+
+	return respone, nil
+}
+
+func decodeCountHTTPRequest(_ context.Context, req *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(req.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, req *nats.Msg) (interface{}, error) {
+	var request countRequest
+	if err := json.Unmarshal(req.Data, &request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func main() {
+	svc := stringService{}
+
+	natsURL := flag.String("nats-url", nats.DefaultURL, "URL for connecting to NATS")
+	listen := flag.String("listen", ":8080", "HTTP Listen Address")
+	flag.Parse()
+
+	nc, err := nats.Connect(*natsURL)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"action": "connect",
+			"url":    natsURL,
+			"err":    err,
+		}).Fatal()
+	}
+	defer nc.Close()
+
+	uppercaseHTTPHandler := httptransport.NewServer(
+		makeUppercaseHTTPEndpoint(nc),
+		decodeUppercaseHTTPRequest,
+		httptransport.EncodeJSONResponse,
+	)
+	countHTTPHandler := httptransport.NewServer(
+		makeUppercaseHTTPEndpoint(nc),
+		decodeCountHTTPRequest,
+		httptransport.EncodeJSONResponse,
+	)
+
+	uppercaseHandler := natstransport.NewSubscriber(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		natstransport.EncodeJSONResponse,
+	)
+
+	countHandler := natstransport.NewSubscriber(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		natstransport.EncodeJSONResponse,
+	)
+
+	uSub, err := nc.QueueSubscribe("stringsvc.uppercase", "stringsvc", uppercaseHandler.ServeMsg(nc))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err":    err,
+			"action": "subscribe",
+		}).Fatal()
+	}
+	defer uSub.Unsubscribe()
+
+	cSub, err := nc.QueueSubscribe("stringsvc.uppercase", "stringsvc", countHandler.ServeMsg(nc))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err":    err,
+			"action": "subscribe",
+		}).Fatal()
+	}
+	defer cSub.Unsubscribe()
+
+	http.Handle("/uppercase", uppercaseHTTPHandler)
+	http.Handle("/count", countHTTPHandler)
+	log.WithFields(log.Fields{
+		"event": "Running Server",
+		"addr": *listen,
+	}).Info()
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
